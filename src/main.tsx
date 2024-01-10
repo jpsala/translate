f9e4f32681fcf4cdf3d9add65a265f82b4fb256c
Juan Pablo Sala <jpsala@gmail.com>
2024-01-10 10:05:13 -0300

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { AppTheme } from './theme/AppTheme'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppTheme>
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
     </AppTheme>
   </React.StrictMode>,
 )
